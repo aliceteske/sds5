59ee471101b06ef188d47ae1f6cbcbf78815685b
Alice Teske <aliceteske@hotmail.com>
2021-11-02 22:38:50 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/aliceteske" target="_blank" rel="noreferrer">Alice Teske</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/teske.alice" target="_blank" rel="noreferrer">@teske.alice</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
